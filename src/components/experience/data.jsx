8c16b17e4ec4cfb6190ad0621a39dde125c3b210
roger <guanchernglin@gmail.com>
2024-01-19 02:47:33 -0500

    image error

--- src/components/experience/data.jsx
+++ src/components/experience/data.jsx
@@ -3,7 +3,7 @@ export const resume = [
     {
       id: 1,
       category: 'experience',
-      icon: <img src="/public/work-solid.svg" alt="" />,
+      icon: <img src="/work-solid.svg" alt="" />,
       year: 'June 2022 - PRESENT',
       title: 'Summer Intern / Industry Control System Security Engineer (Trainee) <span> <br>Institute for Information Industry in Taiwan </span>',
       desc: '',
@@ -12,7 +12,7 @@ export const resume = [
     {
       id: 2,
       category: 'experience',
-      icon: <img src="/public/work-solid.svg" alt="" />,
+      icon: <img src="/work-solid.svg" alt="" />,
       year: 'Aug. 2020 - Aug. 2021',
       title: 'Overseas International Ambassador <span><br> Skyline, An International Opportunities Sharing Platform in Taiwan   </span>',
       desc: '',
@@ -21,7 +21,7 @@ export const resume = [
     {
       id: 3,
       category: 'experience',
-      icon: <img src="/public/work-solid.svg" alt="" />,
+      icon: <img src="/work-solid.svg" alt="" />,
       year: 'July 2019 - Sept. 2019',
       title: 'Summer RD Intern <span> <br> Taiwan Semiconductor Manufacturing Company (TSMC) in Taiwan </span>',
       desc: '',
@@ -30,7 +30,7 @@ export const resume = [
     {
       id: 4,
       category: 'education',
-      icon: <img src="/public/graduation-cap-solid.svg" alt="" />,
+      icon: <img src="/graduation-cap-solid.svg" alt="" />,
       year: 'Sept. 2019 - PRESENT',
       title: 'MS in Computer Science <span> New York University Tandon School of Engineering </span>',
       desc: '',
@@ -39,7 +39,7 @@ export const resume = [
     {
       id: 5,
       category: 'education',
-      icon: <img src="/public/graduation-cap-solid.svg" alt="" />,
+      icon: <img src="/graduation-cap-solid.svg" alt="" />,
       year: 'Sept. 2017 - Nov. 2021',
       title: 'BEng in Computer Science <span> The Hong Kong University of Science and Technology  </span>',
       desc: 'Second Class Honors, Division I, November 2021; Minor in Business',
@@ -48,7 +48,7 @@ export const resume = [
     {
       id: 6,
       category: 'education',
-      icon: <img src="/public/graduation-cap-solid.svg" alt="" />,
+      icon: <img src="/graduation-cap-solid.svg" alt="" />,
       year: 'July 2019 - Aug. 2019',
       title: 'Summer Exchange Student <span> State University of New York at Stony Brook </span>',
       desc: 'Recipient of Summer Exchange Scholarship. Global Summer Institute, August 2019',
