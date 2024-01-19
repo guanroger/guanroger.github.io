f1f1ee59432d3b0a3680d050a8abc4b24e95dca0
roger <guanchernglin@gmail.com>
2024-01-19 01:57:50 -0500

    minor error

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -53,10 +53,10 @@ const About = () =>{
                     <motion.h4 variants={textVariants}>Hi, I’m Fred, aka “Taipei Tree Frog”. I am from Taiwan, and I completed my Bachelor's degree in Computer Science at HKUST. Currently, I am a graduate student at Purdue University pursuing my MSCS degree. My specialties and interests range from full-stack development, UI/UX design, cloud computing and data engineering. I’m passionate about building real-world applications. Being a software developer, I can easily make the world better by making things that are useful to other people. It’s always a great accomplishment to see other people using the product that has my code in it. If you’d like to get in touch, feel free to contact me at any time. </motion.h4>
                     <motion.div variants={textVariants} className="buttons">
                         <motion.a variants={textVariants} href="mailto: guanchernglin@gmail.com" >
-                            <motion.img variants={textVariants} src="/public/envelope-solid-w.svg" alt="" />
+                            <motion.img variants={textVariants} src="\public\envelope-solid-w.svg" alt="" />
                         </motion.a>
                         <motion.a variants={textVariants} href="https://www.linkedin.com/in/guan-cherng-lin-roger-3128b019a/">
-                            <motion.img  variants={textVariants} src="/public/linkedin-w.svg" alt="" />
+                            <motion.img  variants={textVariants} src="\public\linkedin-w.svg" alt="" />
                         </motion.a>
                         <motion.a  variants={textVariants} href="https://github.com/guanroger">
                             <motion.img  variants={textVariants} src="/public/github-w.svg" alt="" />
