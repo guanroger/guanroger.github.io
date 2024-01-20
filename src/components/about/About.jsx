0e825a73f33e5e77e200dd4970aae0ee64e7edfd
roger <guanchernglin@gmail.com>
2024-01-20 15:44:43 -0500

    change bio

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -56,7 +56,12 @@ const About = () =>{
                     <motion.h2 variants={textVariants}> About Me</motion.h2>
                     <motion.h3 variants={textVariants}> Guan-Cherng LIN, Roger</motion.h3>
                     <motion.h1 variants={textVariants}> Master's in CS @ NYU Tandon</motion.h1>
-                    <motion.h4 variants={textVariants}>Hi, I’m Fred, aka “Taipei Tree Frog”. I am from Taiwan, and I completed my Bachelor's degree in Computer Science at HKUST. Currently, I am a graduate student at Purdue University pursuing my MSCS degree. My specialties and interests range from full-stack development, UI/UX design, cloud computing and data engineering. I’m passionate about building real-world applications. Being a software developer, I can easily make the world better by making things that are useful to other people. It’s always a great accomplishment to see other people using the product that has my code in it. If you’d like to get in touch, feel free to contact me at any time. </motion.h4>
+                    <motion.h4 variants={textVariants}>Hi, I’m Guan-Cherng LIN (Roger). I am from Taiwan, and I completed my Bachelor's degree in Computer Science at HKUST. 
+                    I am currently a graduate student at NYU Tandon pursuing my MSCS degree. My specialties and interests range from UI/UX design, Game Engineering and Compter Graphic. 
+                    I'm passionate about creating impactful real-world applications. As a software developer, I find joy in contributing to the betterment of the world by crafting useful solutions for others. 
+                    Witnessing people use products with my code is a fulfilling accomplishment.
+                    I'm also driven by a passion for continuous learning and embracing new technologies to enhance my skills. Crafting solutions that others find valuable is a source of great accomplishment for me.
+                    If you’d like to get in touch, feel free to contact me at any time. I'm always open for connections.</motion.h4>
                     <motion.div variants={textVariants} className="buttons">
                         <motion.a variants={textVariants} href="mailto: guanchernglin@gmail.com" >
                             <motion.img variants={textVariants} src="/svg/envelope-solid-w.svg" alt="" />
