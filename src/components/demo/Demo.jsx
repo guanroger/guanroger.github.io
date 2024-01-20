29d7969b74a6d44ea77e9e59788977afe2c74799
roger <guanchernglin@gmail.com>
2024-01-19 20:06:08 -0500

    add changes to the demo

--- src/components/demo/Demo.jsx
+++ src/components/demo/Demo.jsx
@@ -5,27 +5,31 @@ import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 const items = [
   {
     id: 1,
-    title: "React Commerce",
-    img: "https://images.pexels.com/photos/18073372/pexels-photo-18073372/free-photo-of-young-man-sitting-in-a-car-on-a-night-street.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    title: "NYU Swiftpick",
+    img: "/public/demo/swiftpick.png",
+    desc: "This is the final result of NYU CS-GY 6513 Big Data Course. We build a website that can give the driver to find the best pickup spot in New York. We use the past Taxi Data captured in NY, with the Big Data knowledge we learned and the past experiences we have (Frontend/Backend & Custering to finish it.",
+    demo:"https://swiftpick-bigdata-project-online.onrender.com/",
   },
   {
     id: 2,
-    title: "Next.js Blog",
-    img: "https://images.pexels.com/photos/18023772/pexels-photo-18023772/free-photo-of-close-up-of-a-person-holding-a-wristwatch.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    title: "HKUST Human Computer Interaction Final Personal Project",
+    img: "/public/demo/HCI-web.jpg",
+    desc: "This website is my pervious personal website. In it was the 3 projects that i have idealize with my classmates using the HCI techniques learned in the HCI course",
+    demo:"https://guanchernglin.wixsite.com/website",
   },
   {
     id: 3,
-    title: "Vanilla JS App",
-    img: "https://images.pexels.com/photos/6894528/pexels-photo-6894528.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    title: "HKUST Game Programming Final Project - 2D Platform Action Game",
+    img: "/public/demo/2Dgameplay.png",
+    desc: "Cooperated with groupmates to develop a small 2D platform action game using Unity and C. Designed key-binding system for players to move around and shoot enemies, a skill tree system to update status and upgrade weapons. Also designed various levels and difficulties for maps. Innovated and designed the whole concept of the game world, game narrative arcs, features, and scenarios for gameplay",
+    demo: ""
   },
   {
     id: 4,
-    title: "Music App",
-    img: "https://images.pexels.com/photos/18540208/pexels-photo-18540208/free-photo-of-wood-landscape-water-hill.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores ab id ad nesciunt quo aut corporis modi? Voluptate, quos sunt dolorum facilis, id eum sequi placeat accusantium saepe eos laborum.",
+    title: "HKUST UChoir Offical Website",
+    img: "/public/demo/hkustchoir.png",
+    desc: "First time using HTML/CSS, Cooperated with a senior student in UChoir to build an interactive website.",
+    demo:"https://choir.su.hkust.edu.hk/"
   },
 ];
 
@@ -49,7 +53,7 @@ const Single = ({ item }) => {
           <motion.div className="textContainer" style={{y}}>
             <h2>{item.title}</h2>
             <p>{item.desc}</p>
-            <button>See Demo</button>
+            <a href={item.demo}><button>See Demo</button></a>
           </motion.div>
         </div>
       </div>
