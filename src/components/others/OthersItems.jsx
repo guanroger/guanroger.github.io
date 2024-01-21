e5a76c2fa1f7eb6b0e7bd510601545f4af10cde6
roger <guanchernglin@gmail.com>
2024-01-20 22:08:33 -0500

    fix mobile

---
+++ src/components/others/OthersItems.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const OthersItem =(img, title, detail) =>{
+    return (
+    <div >
+        <img src={img} alt="" className="other_image" />
+
+        
+
+        
+    </div>
+    )
+}
+
+export default OthersItem
